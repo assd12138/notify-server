005cd1b0a17120290195b2cd052704c3a6ba3022
assd12138 <1796684119@qq.com>
2022-01-27 21:14:21 +0800

    天气没有

--- src/api/loveMsg.ts
+++ src/api/loveMsg.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { getTian } from '../utils/http'
 const {WEATHER_SECRET_KEY} = process.env
+console.log('天气api' + WEATHER_SECRET_KEY);
+
 /**
  * 给女朋友发送内容的相关接口
  */
